038bcf3dc7eb40c838a6431aad9588cac61b0263
Edward Salter <edwardsalter@live.co.uk>
2022-12-15 16:36:27 +0000

    Removed logging and set content type to atom xml.

--- Feed/index.js
+++ Feed/index.js
@@ -4,9 +4,11 @@ module.exports = async function (context) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
   const feed = await generateFeed();
-  console.log(feed);
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: feed,
+    headers: {
+      ["Content-Type"]: "application/atom+xml",
+    },
   };
 };
