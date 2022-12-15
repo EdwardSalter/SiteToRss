67d5a174fbbd1d2d71367045c6bd071b7b58e8a9
Edward Salter <edwardsalter@live.co.uk>
2022-12-15 14:51:38 +0000

    Fixed case of author element in entries. Added link element to entries. Added additional link element to root with link to hobbycomps website and changed original link element to point to self.

--- feed.js
+++ feed.js
@@ -1,123 +1,142 @@
-const {load} = require('cheerio');
-const XMLWriter = require('xml-writer');
-const {fetch} = require('cross-fetch');
+const { load } = require("cheerio");
+const XMLWriter = require("xml-writer");
+const { fetch } = require("cross-fetch");
 
-const URL = 'https://www.hobbycomps.co.uk/competitions/';
+const URL = "https://www.hobbycomps.co.uk/competitions/";
 
-const PRODUCT_SELECTOR = '.product';
-const PRODUCT_TITLE_SELECTOR = '.woocommerce-loop-product__title';
-const PRODUCT_PRICE_SELECTOR = '.price';
-const PRODUCT_IMAGE_SELECTOR = 'img';
-const PRODUCT_LINK_SELECTOR = 'a';
-const PRODUCT_TIME_SELECTOR = '.lty-lottery-countdown-timer';
-
+const PRODUCT_SELECTOR = ".product";
+const PRODUCT_TITLE_SELECTOR = ".woocommerce-loop-product__title";
+const PRODUCT_PRICE_SELECTOR = ".price";
+const PRODUCT_IMAGE_SELECTOR = "img";
+const PRODUCT_LINK_SELECTOR = "a";
+const PRODUCT_TIME_SELECTOR = ".lty-lottery-countdown-timer";
 
 async function getProductInfo() {
-    const response = await fetch(URL);
-    const page = await response.text();
+  const response = await fetch(URL);
+  const page = await response.text();
 
-    const $ = load(page);
+  const $ = load(page);
 
-    const products = $(PRODUCT_SELECTOR);
+  const products = $(PRODUCT_SELECTOR);
 
-    return products.map(function () {
-        const $el = $(this);
-        const title = $el.find(PRODUCT_TITLE_SELECTOR).text();
-        const price = $el.find(PRODUCT_PRICE_SELECTOR).text();
-        const image = $el.find(PRODUCT_IMAGE_SELECTOR).attr('src');
-        const link = $el.find(PRODUCT_LINK_SELECTOR).attr('href');
-        const time = new Date($el.find(PRODUCT_TIME_SELECTOR).data('time'));
+  return products
+    .map(function () {
+      const $el = $(this);
+      const title = $el.find(PRODUCT_TITLE_SELECTOR).text();
+      const price = $el.find(PRODUCT_PRICE_SELECTOR).text();
+      const image = $el.find(PRODUCT_IMAGE_SELECTOR).attr("src");
+      const link = $el.find(PRODUCT_LINK_SELECTOR).attr("href");
+      const time = new Date($el.find(PRODUCT_TIME_SELECTOR).data("time"));
 
-        return {
-            title,
-            price,
-            image,
-            link,
-            date: time,
-        }
-    }).get();
+      return {
+        title,
+        price,
+        image,
+        link,
+        date: time,
+      };
+    })
+    .get();
 }
 
 async function generateFeed() {
-    const products = await getProductInfo();
+  const products = await getProductInfo();
 
-    const xw = new XMLWriter;
-    const doc = xw.startDocument();
-    const feed = doc.startElement('feed');
-    feed.writeAttribute('xmlns', "http://www.w3.org/2005/Atom");
-    feed.writeAttribute('xmlns:media', "http://search.yahoo.com/mrss/")
+  const xw = new XMLWriter();
+  const doc = xw.startDocument();
+  const feed = doc.startElement("feed");
+  feed.writeAttribute("xmlns", "http://www.w3.org/2005/Atom");
+  feed.writeAttribute("xmlns:media", "http://search.yahoo.com/mrss/");
 
-    const link = feed.startElement('link');
-    link.writeAttribute('href', 'https://www.hobbycomp.co.uk');
+  const link = feed.startElement("link");
+  link.writeAttribute("rel", "self");
+  link.writeAttribute(
+    "href",
+    "https://edsalter-rss.azurewebsites.net/api/feed"
+  );
+  link.endElement();
+
+  feed.writeElement("id", "HobbyComp");
+
+  feed.writeElement("title", "HobbyComp Competitions");
+
+  const link2 = feed.startElement("link");
+  link2.writeAttribute("rel", "alternate");
+  link2.writeAttribute("href", "https://www.hobbycomp.co.uk");
+  link2.endElement();
+
+  const author = feed.startElement("author");
+  author.writeElement("name", "HobbyComp");
+  author.writeElement("uri", "https://www.hobbycomp.co.uk");
+  author.endElement();
+
+  feed.writeElement("published", new Date().toISOString());
+
+  products.forEach((p) => {
+    const entry = feed.startElement("entry");
+    entry.writeElement("id", p.title);
+    entry.writeElement("title", p.title);
+
+    const link = entry.startElement("link");
+    link.writeAttribute("rel", "alternate");
+    link.writeAttribute("href", p.link);
     link.endElement();
 
-    feed.writeElement('title', "HobbyComp Competitions");
+    const entryAuthor = entry.startElement("author");
+    entryAuthor.writeElement("name", "HobbyComps");
+    entryAuthor.writeElement("uri", "https://www.hobbycomp.co.uk");
+    entryAuthor.endElement();
 
-    const author = feed.startElement('author');
-    author.writeElement('name', 'HobbyComp');
-    author.writeElement('uri', 'https://www.hobbycomp.co.uk');
-    author.endElement();
+    entry.writeElement(
+      "published",
+      new Date("2022-12-05T13:39:10.000Z").toISOString()
+    );
 
-    feed.writeElement('id', 'HobbyComp');
-    feed.writeElement('published', new Date().toISOString());
+    const media = entry.startElement("media:group");
+    media.writeElement("media:title", p.title);
+    const content = media.startElement("media:content");
+    content.writeAttribute("url", p.image);
+    content.writeAttribute("type", "image/png");
+    content.writeAttribute("width", "324");
+    content.writeAttribute("height", "324");
+    content.endElement();
 
+    const thumbnail = media.startElement("media:thumbnail");
+    thumbnail.writeAttribute("url", p.image);
+    thumbnail.writeAttribute("width", "324");
+    thumbnail.writeAttribute("height", "324");
+    thumbnail.endElement();
 
-    products.forEach((p) => {
-        const entry = feed.startElement('entry');
-        entry.writeElement('id', p.title);
-        entry.writeElement('title', p.title);
-        const entryAuthor = entry.startElement('Author');
-        entryAuthor.writeElement('name', 'HobbyComp');
-        entryAuthor.writeElement('uri', 'https://www.hobbycomp.co.uk');
-        entryAuthor.endElement();
-        entry.writeElement('published', new Date().toISOString());
-        const media = entry.startElement('media:group');
-        media.writeElement('media:title', p.title);
-        const content = media.startElement('media:content');
-        content.writeAttribute('url', p.image);
-        content.writeAttribute('type', 'image/png');
-        content.writeAttribute('width', '324');
-        content.writeAttribute('height', '324');
-        content.endElement();
+    media.writeElement("media:description", p.price);
 
-        const thumbnail = media.startElement('media:thumbnail');
-        thumbnail.writeAttribute('url', p.image);
-        thumbnail.writeAttribute('width', '324');
-        thumbnail.writeAttribute('height', '324');
-        thumbnail.endElement();
+    media.endElement();
+    entry.endElement();
+  });
 
-        media.writeElement('media:description', p.price);
+  feed.endElement();
+  doc.endDocument();
 
-        media.endElement();
-        entry.endElement();
-    })
+  return xw.toString();
 
+  // const feed = new Feed({
+  //     title: 'HobbyComp Competitions',
+  //     description: 'Active competitions currently listed on HobbyComp.co.uk',
+  //     link: 'https://www.hobbycomp.co.uk',
+  //     id: 'https://www.hobbycomp.co.uk',
+  //     copyright: 'Hobby Comps 2022',
+  // });
 
-    feed.endElement();
-    doc.endDocument();
-
-    return xw.toString();
-
-
-    // const feed = new Feed({
-    //     title: 'HobbyComp Competitions',
-    //     description: 'Active competitions currently listed on HobbyComp.co.uk',
-    //     link: 'https://www.hobbycomp.co.uk',
-    //     id: 'https://www.hobbycomp.co.uk',
-    //     copyright: 'Hobby Comps 2022',
-    // });
-
-    // products.forEach((p) => {
-    //     feed.addItem({
-    //         ...p,
-    //         description: p.price,
-    //         content: p.image,
-    //     })
-    // })
-    //
-    // return feed.atom1();
+  // products.forEach((p) => {
+  //     feed.addItem({
+  //         ...p,
+  //         description: p.price,
+  //         content: p.image,
+  //     })
+  // })
+  //
+  // return feed.atom1();
 }
 
 module.exports = {
-    generateFeed
-}
+  generateFeed,
+};
